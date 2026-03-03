e8ab3d5502cf99b071bb061a634eebc6658a87f1
Matt Bolaños <69167336+mattbolanos@users.noreply.github.com>
2026-03-02 18:38:39 -0800

    Update page.tsx

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -52,7 +52,7 @@ export default async function ProjectPage({
   if (!project) notFound();
 
   return (
-    <div className="space-y-8">
+    <div className="space-y-9">
       <section className="space-y-3">
         <div className="flex items-center gap-x-3">
           <Image
