2a3a7cad3a35260e028b074a65f0b70f7848e6b9
mattbolanos <matthew.a.bolanos@gmail.com>
2026-03-12 13:10:52 -0400

    don't need image config anymore

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  images: {
-    remotePatterns: [
-      {
-        hostname: "lastfm.freetls.fastly.net",
-        protocol: "https",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
