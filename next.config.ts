7e31676bda5f8cadd116c0163b861360cd11bc4b
mattbolanos <matthew.a.bolanos@gmail.com>
2026-04-15 23:03:03 -0400

    fix: rm unused turbopack loader + make stratiles smaller

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  turbopack: {
-    rules: {
-      "*.md": {
-        as: "*.js",
-        loaders: ["raw-loader"],
-      },
-    },
-  },
   typedRoutes: true,
 };
 
