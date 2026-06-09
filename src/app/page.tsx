8acfc7d8dc7d1fc8964b7e3fb1cf8d4e6691c9ac
mattbolanos <matthew.a.bolanos@gmail.com>
2026-06-08 23:52:43 -0400

    add more files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Intro } from "@/app/intro";
+import { Experience } from "./experience";
 
 export default function Home() {
   return (
     <div className="space-y-10">
       <Intro />
+
+      <Experience />
     </div>
   );
 }
