3f2965b42a86a2db498e8476d3440d53e3448645
mattbolanos <matthew.a.bolanos@gmail.com>
2026-06-15 10:56:19 -0700

    Simplify Suspense fallbacks on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Suspense } from "react";
 import { Intro } from "@/app/intro";
 import { Heatmap } from "@/components/heatmap";
 import { HeatmapSkeleton } from "@/components/heatmap/skeleton";
-import { ResponsiveViewTransition } from "@/components/responsive-view-transition";
 import { RecentRuns } from "@/components/strava/recent-runs";
 import { StravaSkeleton } from "@/components/strava/skeleton";
 import { getGithubContributions } from "@/lib/api/github";
@@ -28,32 +27,16 @@ export default function Home() {
         <section className="space-y-3">
           <h2>Running</h2>
           <div className="heatmap-container">
-            <Suspense
-              fallback={
-                <ResponsiveViewTransition exit="slide-down">
-                  <StravaSkeleton />
-                </ResponsiveViewTransition>
-              }
-            >
-              <ResponsiveViewTransition default="none" enter="slide-up">
-                <StravaWrapper />
-              </ResponsiveViewTransition>
+            <Suspense fallback={<StravaSkeleton />}>
+              <StravaWrapper />
             </Suspense>
           </div>
         </section>
         <section className="space-y-3">
           <h2>Coding</h2>
           <div className="heatmap-container">
-            <Suspense
-              fallback={
-                <ResponsiveViewTransition exit="slide-down">
-                  <HeatmapSkeleton />
-                </ResponsiveViewTransition>
-              }
-            >
-              <ResponsiveViewTransition default="none" enter="slide-up">
-                <GitHubWrapper />
-              </ResponsiveViewTransition>
+            <Suspense fallback={<HeatmapSkeleton />}>
+              <GitHubWrapper />
             </Suspense>
           </div>
         </section>
